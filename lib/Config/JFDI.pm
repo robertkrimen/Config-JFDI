7f4e1fb6b9bd957095cd744b71dd9b404a3e3f7f
robertkrimen <robertkrimen@gmail.com>
2009-05-14 13:27:49 -0700

    Forgot some dependencies in Makefile.PL

--- lib/Config/JFDI.pm
+++ lib/Config/JFDI.pm
@@ -9,11 +9,11 @@ Config::JFDI - Just * Do it: A Catalyst::Plugin::ConfigLoader-style layer over C
 
 =head1 VERSION
 
-Version 0.05_1
+Version 0.05_2
 
 =cut
 
-our $VERSION = '0.05_1';
+our $VERSION = '0.05_2';
 
 =head1 SYNPOSIS 
 
