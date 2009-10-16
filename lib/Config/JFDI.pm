ca30fb67d0d785046059db0800bbd80d7c3db2a3
robertkrimen <robertkrimen@gmail.com>
2009-10-16 00:52:28 -0700

    Version bump to _3

--- lib/Config/JFDI.pm
+++ lib/Config/JFDI.pm
@@ -9,11 +9,11 @@ Config::JFDI - Just * Do it: A Catalyst::Plugin::ConfigLoader-style layer over C
 
 =head1 VERSION
 
-Version 0.063_2
+Version 0.063_3
 
 =cut
 
-our $VERSION = '0.063_2';
+our $VERSION = '0.063_3';
 
 =head1 SYNPOSIS 
 
